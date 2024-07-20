c5568d977a2e6b63e6ab3f95b0060124a963917f
KeyurMistry123 <keyur04mistry@gmail.com>
2024-07-20 14:59:24 +0530

    video_screen page commit

--- lib/videos_screen.dart
+++ lib/videos_screen.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
 
 class YouTubeLinksScreen extends StatelessWidget {
-  // Function to navigate to different pages based on the container tapped
-  void _onContainerTapped(BuildContext context, String title) {
-    // Implement navigation or any other action here
-    // For example, using Navigator.push to navigate to a different screen
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => DetailsPage(title: title),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,54 +19,39 @@ class YouTubeLinksScreen extends StatelessWidget {
   }
 
   Widget _buildInfoContainer(BuildContext context, String title) {
+    String imagePath;
+    switch (title) {
+      case 'Crop Selection':
+        imagePath = 'assets/images/crop_selection.jpg';
+        break;
+      case 'Pesticide Side Effects':
+        imagePath = 'assets/images/pesticide_side_effects.jpg';
+        break;
+      case 'Agricultural Techniques':
+        imagePath = 'assets/images/agricultural_techniques.jpg';
+        break;
+      case 'Fertilizers':
+        imagePath = 'assets/images/fertilizers.jpg';
+        break;
+      default:
+        imagePath = 'assets/images/default.jpg'; // Default image if no match
+    }
+
     return InkWell(
       onTap: () => _onContainerTapped(context, title),
       child: Container(
         padding: EdgeInsets.all(16),
-        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 4,
-              offset: Offset(0, 2),
-            )
-          ],
-        ),
-        child: Center(
-          child: Text(
-            title,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
-          ),
-        ),
+        // ... existing code ...
       ),
     );
   }
-}
 
-// Example of a details page that can be navigated to from the info containers
-class DetailsPage extends StatelessWidget {
-  final String title;
-
-  DetailsPage({required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Details for $title',
-          style: TextStyle(fontSize: 24),
-        ),
+  // Function to navigate to different pages based on the container tapped
+  void _onContainerTapped(BuildContext context, String title) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => YouTubeLinksScreen(title: title),
       ),
     );
   }
